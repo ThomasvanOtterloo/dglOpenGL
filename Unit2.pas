5b5ceb80c899f9f38703ccb73dc121a8b7fdc87a
thomas vanotterloo <vanotterloo.thomas@gmail.com>
2023-10-12 16:28:08 +0200

    Tutorial 2
    Triangle/square with colors on screen

--- Unit2.pas
+++ Unit2.pas
@@ -127,6 +127,14 @@ begin
   glVertex3f(0, 1, 0);
   glend;
 
+  //triangle
+  glBegin(GL_TRIANGLES);
+  glColor3f(1,0,0); glVertex3f(-1,-1, 0);
+  glColor3f(1,0,1); glVertex3f( 1,-1, 0);
+  glColor3f(1,1,0); glVertex3f( 0, 1, 0);
+  glend;
+
+
   SwapBuffers(DC);
 end;
 
